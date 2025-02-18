7831465a2ba3238f0ac181240dded5a0912caf2b
unknown <thapliyalarpan@gmail.com>
2025-02-18 11:20:41 +0530

    finished

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Typescript_bridgelabz
+- Introduction
+
+- Technologies Used
+
+    - TypeScript
+
+    - Node.js
+
+    - Core TypeScript Concepts
+
+    - Tuples
+
+    - Enums
+
+    - Any vs Unknown
+
+    - Void, Null, and Undefined
+
+    - Never
+
+    - Interfaces & Type Extensions
+
+    - Types & Type Operations
+
+    - Classes and Objects
+
+    - Callbacks & Functions
+
+    - Rest Parameters
+
+    - Generics
--- app.js
+++ app.js
@@ -61,32 +61,81 @@
 // type admin = user & {
 //     abc(newUser:string) :void
 // }
+{
+    // intersection vs types
+    // 1. only intersection can extends
+    // 2. can no use multiple type of same name
+    // 3. in intersection we cannt use type union or intersection 
+}
 // function make (obj :admin){
 //     obj.abc('mop');
 // }
 // classes and objects
-class user {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-    get getname() {
-        return this.name;
-    }
-    set setname(val) {
-        this.name = val;
-    }
+// class user{
+//     private name;
+//     public readonly age;
+//     protected gender;
+//     static version = '10.0.1';
+//      constructor(name:string,age :number,gender?:string){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender
+//      }
+//       get getname(){
+//         return this.name;
+//       }
+//       set setname(val:string){
+//         this.name = val;
+//       }
+// }
+// class semiuser extends user{
+//     constructor(name:string, age:number, gender?:string){
+//         super(name,age,gender);
+//     }
+//     get getsemiuser(){
+//         return this.age;
+//     }
+// }
+// let u1 = new user('rock',30,"m");
+// let su1 = new semiuser('popoye',40);
+// console.log(u1.age);
+// console.log(su1.age);
+// console.log(user.version);
+//call-back function
+// function abc (name:string, call : (val:string)=>void){
+//     call('dawg');
+// }
+// abc('joseph',(val:string)=>{
+//     console.log(val);
+// })
+//rest 
+// function abc(...args : number[]) { }
+// abc(1,2,3,4,5,6,7,8);
+//generics
+// function
+// function abc<T>(a:<T>,b:number){
+// }
+// interface
+// interface user<T>{
+//     name:T;
+//     age:number;
+// }
+// function abc(a:user<string>){
+//     console.log(a);
+// }
+// abc({name:"sam",age:12});
+//class
+// class user<T>{
+//     constructor(a:T,b:string){
+//         console.log(typeof a);
+//     }
+// }
+// let u1 = new user(1001,"ram");
+//edge case (cannot return string as it can ce of any type rather than string)
+function ab(a, b) {
+    console.log(a);
+    return "bob";
+    return "bob";
+    return "bob";
 }
-class semiuser extends user {
-    constructor(name, age, gender) {
-        super(name, age, gender);
-    }
-    get getsemiuser() {
-        return this.age;
-    }
-}
-let u1 = new user('rock', 30, "m");
-let su1 = new semiuser('popoye', 40);
-console.log(u1.age);
-console.log(u1.age);
+console.log(ab(1, 2));
